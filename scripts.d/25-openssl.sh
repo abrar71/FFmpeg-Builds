fe6342db54cf7f47f5c88a31bf909cde3caadeb0
Abrar Ahmed <a@abrar.me>
2023-05-02 08:23:48 +0000

    openssl 3.1.0

--- scripts.d/25-openssl.sh
+++ scripts.d/25-openssl.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 SCRIPT_REPO="https://github.com/openssl/openssl.git"
-SCRIPT_COMMIT="openssl-3.0.8"
-SCRIPT_TAGFILTER="openssl-3.0.*"
+SCRIPT_COMMIT="openssl-3.1.0"
+SCRIPT_TAGFILTER="openssl-3.1.*"
 
 ffbuild_enabled() {
     return 0
