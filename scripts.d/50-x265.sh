287ffef47e4f6a835dd66fc1df0821f4429a9bc8
Abrar Ahmed <13979201+abrar71@users.noreply.github.com>
2024-04-15 11:37:40 +0000

    merge_0ee6cfa

--- scripts.d/50-x265.sh
+++ scripts.d/50-x265.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCRIPT_REPO="https://bitbucket.org/multicoreware/x265_git.git"
-SCRIPT_COMMIT="3cf6c1e53037eb9e198860365712e1bafb22f7c6"
+SCRIPT_COMMIT="5a6d85d768699c36fe810c40be89db82033f6743"
 
 ffbuild_enabled() {
     [[ $VARIANT == lgpl* ]] && return -1
@@ -45,7 +45,7 @@ EOF
         mv ../10bit/libx265.a ../8bit/libx265_main10.a
         mv libx265.a libx265_main.a
 
-        ${FFBUILD_CROSS_PREFIX}ar -M <<EOF
+        ${AR} -M <<EOF
 CREATE libx265.a
 ADDLIB libx265_main.a
 ADDLIB libx265_main10.a
